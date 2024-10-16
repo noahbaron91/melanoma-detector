1f928aebe771c8150e0a9a47246a8b5a7784e6e5
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-10-16 14:03:30 -0700

    fix

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from fastai.vision.all import load_learner
 import pickle
+from PIL import Image
+import io
+import os
+from flask import request, current_app
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app) 
@@ -14,7 +19,7 @@ def get_y(r):
   return r['benign_malignant']
 
 try:
-    learn_inf = load_learner('../models/model-1.pkl')
+    learn_inf = load_learner('../models/model-2.pkl')
     print('Model loaded successfully!')
 except Exception as e:
     print(f'Error loading model: {str(e)}')
@@ -22,13 +27,21 @@ except Exception as e:
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    results = learn_inf.predict('../images/ISIC_0085644.jpg')
-    print(results)
-    # Add your prediction logic here
-    # For example:
-    # image = request.files['image']
-    # prediction = learner.predict(image)
-    # return jsonify({'prediction': prediction})
+    image = request.files['image']
+
+    upload_dir = os.path.join(current_app.root_path, 'uploads')
+    os.makedirs(upload_dir, exist_ok=True)
+    
+    # Generate a unique filename
+    filename = secure_filename(image.filename)
+    
+    # Save the file
+    file_path = os.path.join(upload_dir, filename)
+    image.save(file_path)
+
+    print(f'File saved to {file_path}')
+    results = learn_inf.predict(file_path)
+  
     return {'success': True, 'data': {'classification': results[0]}}
 
 if __name__ == '__main__':
