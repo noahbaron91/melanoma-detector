b6ca7da4c42768bc515ee3a52f451a4b86ff8854
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-10-16 12:32:26 -0700

    feat: initial

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from fastai.vision.all import load_learner
+import pickle
+
+app = Flask(__name__)
+
+
+def get_x(r):
+  return f'images/{r["isic_id"]}.jpg'
+
+def get_y(r):
+  return r['benign_malignant']
+
+try:
+    learn_inf = load_learner('../models/model-1.pkl')
+    print('Model loaded successfully!')
+except Exception as e:
+    print(f'Error loading model: {str(e)}')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    results = learn_inf.predict('../images/ISIC_0085644.jpg')
+    print(results)
+    # Add your prediction logic here
+    # For example:
+    # image = request.files['image']
+    # prediction = learner.predict(image)
+    # return jsonify({'prediction': prediction})
+    return {'success': True, 'data': {'classification': results[0]}}
+
+if __name__ == '__main__':
+    app.run(debug=True)
